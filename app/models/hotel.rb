25b9c25b53551a031f980b75ed73ca1c9f420345
Petter Fagerlund <petter.fagerlund@gmail.com>
2017-12-30 12:36:32 -0500

    Added herbgem for rails console & created seedfile

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,2 +1,14 @@
 class Hotel < ApplicationRecord
+  # def  self.welcome_message #in the rails consolo Hotel.welcome_message will display the message (dont forget to reload!)
+  #   "Welcome to our hotel."
+  # end
+  #
+  # def room_service # in the rails console -  Hotel.new.room_service (dont forget to reload!)
+  #   "This is room service, how may i help you"
+  # end
+
+
+
+  #require, name for a new hotel.
+  validates :name , presence: true
 end
